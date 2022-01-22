cda671ad1dee7ba097747717bfb20cf7b0edda37
Vineet <vineet98love@gmail.com>
2022-01-22 21:12:56 +0530

    update

---
+++ block-BRaabs/shipping/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import BillingAddress from './components/BillingAddress';
+import ShippingAddress  from './components/ShippingAddress';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <BillingAddress /> */}
+      <ShippingAddress />
+    </div>
+  );
+}
+
+export default App;
